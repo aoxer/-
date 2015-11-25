b692bd50a41b6d1170484196368f7c4507ee49de
aoxer <a@as-MacBook-Pro.local>
2015-11-25 10:38:08 +0800

    Initial Commit

---
+++ 无限滚动/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  无限滚动
+//
+//  Created by a on 15/11/25.
+//  Copyright (c) 2015年 YongChaoAn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
